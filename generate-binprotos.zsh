e24a9df417b6bfc48c3fadc79582ee1e4dfc0dfb
sashaweiss-signal <sasha@signal.org>
2024-05-20 11:38:56 -0700

    Run generator on all jsonprotos

---
+++ generate-binprotos.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+if ! command -v cargo > /dev/null; then
+  echo "Error: cargo not installed!"
+  exit 1
+fi
+
+if [ -z "$TEST_CASE_DIR" ]; then
+  TEST_CASE_DIR="$PWD/test-cases"
+fi
+
+for TEST_CASE_FILE in $(ls $TEST_CASE_DIR); do
+  TEST_CASE_NAME=$(basename $TEST_CASE_FILE .jsonproto)
+
+  cargo run \
+    "$TEST_CASE_DIR/$TEST_CASE_NAME.jsonproto" \
+    > "$TEST_CASE_DIR/$TEST_CASE_NAME.binproto"
+done
