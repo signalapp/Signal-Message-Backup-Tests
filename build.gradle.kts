882fd3a53bf404e620850914d85466347b8e3018
Jordan Rose <jrose@signal.org>
2024-09-12 17:18:05 -0700

    Update to libsignal v0.57.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,6 @@ application {
 }
 
 dependencies {
-  implementation("org.signal:libsignal-client:0.56.1")
+  implementation("org.signal:libsignal-client:0.57.1")
   implementation(kotlin("reflect"))
 }
