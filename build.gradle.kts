3e7e219abd56a05929135afadc32e3a13c2b8506
Jordan Rose <jrose@signal.org>
2024-11-13 13:46:29 -0800

    Update to libsignal v0.62.0 (no changes needed)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ application {
 }
 
 dependencies {
-  implementation("org.signal:libsignal-client:0.60.2")
+  implementation("org.signal:libsignal-client:0.62.0")
   implementation("com.thedeanda:lorem:2.2")
   implementation(kotlin("reflect"))
 }
